814ad082d456325633fe82540449af93929205d2
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-11-06 01:49:11 +0100

    add the ability to filter tasks

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const tasksContainer = document.querySelector(".tasks");
 const newTaskForm = document.querySelector(".new-task-form");
 const newTaskInput = document.querySelector(".new-task-input");
+const filters = document.querySelectorAll(".filter");
 
 let tasks = [
     {
         id: "1",
-        title: "Sleep",
+        title: "Exercise",
         completed: false
     },
     {
@@ -25,24 +26,56 @@ newTaskForm.addEventListener("submit", event => {
 
     const taskName = newTaskInput.value;
 
-    if (taskName === "" || taskName === null) {
-        return;
-    };
+    if (taskName === "" || taskName === null) return;
     
-    
-    
-    
+
     const newTask = new Task(Date.now().toString(), taskName, false);
-    
-    
-    
-    
     
     
     newTaskInput.value = null;
     tasks.push(newTask);
-    render();
+    render(tasks);
 });
+
+
+tasksContainer.addEventListener("click", event => {
+    const targetTagName = event.target.tagName.toLowerCase();
+
+    if (targetTagName === "input") {
+        completeTask(event.target);
+    };
+
+    if (targetTagName === "button") {
+        deleteTask(event.target);
+        render(tasks);
+    };
+});
+
+filters.forEach(filter => {
+    filter.addEventListener("click", event => {
+        removeActiveClass(filters);
+        event.target.classList.add("active");
+
+        if (event.target.innerHTML === "Completed") {
+            const completed = getCompleted(tasks);
+            render(completed);
+        };
+        
+        if (event.target.innerHTML === "Active") {
+            const active = getActive(tasks);
+            render(active);
+        };
+        
+        if (event.target.innerHTML === "All") {
+            render(tasks);
+        };
+    });
+});
+
+render(tasks);
+
+
+
 
 
 // onw module
@@ -54,138 +87,79 @@ class Task {
     };
 };
 
-
-
-    
-
 // own module
-// render tasks
-function render() {
-    clearElement(tasksContainer);
-
-    if (tasks.length === 0) {
-        tasksContainer.innerHTML = "<h1>Add a new task!</h1>"
-    };
-
-    tasks.forEach(task => {
-        tasksContainer.innerHTML += `
-            <div class="task">
-                <div class="task-input">
-                    <input id="${task.id}" type="checkbox" ${task.completed ? "checked" : ""}>
-                    <label class=${task.completed ? "completed" : ""} for="${task.id}">${task.title}</label>
-                </div>
-
-                <button class="btn delete" data-id=${task.id}></button>
-            </div>
-        `;
-    }); 
-};
-
-
-
-
-// own module
-// make elelemnt empty
 function clearElement(element) {
     while (element.firstChild) {
         element.removeChild(element.firstChild);
     };
 };
 
-render();
-
-
-
-
-
 // own module
-// mark tasks as completed
-tasksContainer.addEventListener("click", event => {
-    if (event.target.tagName.toLowerCase() !== "input") {
-        return;
-    };
-
-    const label = event.target.nextElementSibling;
-
+function completeTask(task) {
+    const label = task.nextElementSibling;
+    label.classList.toggle("completed");
+    
     for (let i = 0; i < tasks.length; i++) {
-        if (event.target.id === tasks[i].id) {
+        if (tasks[i].id === task.id) {
             tasks[i].completed = !tasks[i].completed;
-            
-            label.classList.toggle("completed");
         };
     };
-});
+};
 
 // own module
-// delete tasks
-tasksContainer.addEventListener("click", event => {
-    if (event.target.tagName.toLowerCase() !== "button") {
-        return;
-    };
-
+function deleteTask(task) {
     for (let i = 0; i < tasks.length; i++) {
-        if (event.target.dataset.id === tasks[i].id) {
+        if (task.dataset.id === tasks[i].id) {
             tasks.splice(i,1);
-            event.target.remove();
+            task.remove();
         };
-    }
+    };
+};
 
-    render()
-});
+// own module
+function getActive(list) {
+    return list.filter(task => !task.completed)
+};
 
+// own module
+function getCompleted(list) {
+    return list.filter(task => task.completed);
+};
 
+// own module
+function removeActiveClass(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].classList.remove("active");
+    };
+};
 
+// own module
+function render(tasksList) {
+    const tasksContainer = document.querySelector(".tasks");
 
+    clearElement(tasksContainer);
 
+    tasksList.forEach(task => {
+        tasksContainer.innerHTML += `
+            <div class="task">
+                <div class="task-input">
+                    <input
+                        id="${task.id}"
+                        type="checkbox"
+                        ${task.completed ? "checked" : ""}>
+                    
+                    <label 
+                        class=${task.completed ? "completed" : ""}
+                        for="${task.id}">
+                        ${task.title}
+                    </label>
+                </div>
 
-
-
-
-// tasksContainer.addEventListener("click", event => {
-//     if (event.target.tagName.toLowerCase() === "input") {
-//         complete()
-//     };
-
-//     if (event.target.tagName.toLowerCase() === "button") {
-//         delete();
-//     };
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function renderTasks(tasksList) {
-//     const tasksContainer = document.querySelector(".tasks");
-
-//     clearElement(tasksContainer);
-
-//     // if (tasks.length === 0) {
-//     //     tasksContainer.innerHTML = "<h1>Add a new book!</h1>"
-//     // };
-
-//     tasksList.forEach(task => {
-//         tasksContainer.innerHTML += `
-//             <div class="task">
-//                 <div class="task-input">
-//                     <input id="${task.id}" type="checkbox">
-//                     <label for="${task.id}">${task.title}</label>
-//                 </div>
-
-//                 <button class="btn delete">
-//                     <img src="./assets/icon-cross.svg" alt="">
-//                 </button>
-//             </div>
-//         `;
-//     }); 
-// };
+                <button 
+                    class="btn delete"
+                    data-id=${task.id}>
+                </button>
+            </div>
+        `;
+    }); 
+};
